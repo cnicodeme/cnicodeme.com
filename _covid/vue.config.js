20e6a4b0f7142eaf693fa7f22ee226d99c8c6c39
Cyril <contact@cnicodeme.com>
2020-10-30 16:00:37 +0100

    pushing changes live

--- _covid/vue.config.js
+++ _covid/vue.config.js
@@ -1,19 +1,17 @@
 module.exports = {
     configureWebpack: {
-      module: {
-        rules: [
-          {
-            test: /\.pdf$/,
-            use: [
-              {
-                loader: 'url-loader',
-                options: {
-                  name: 'files/[name].[hash:8].[ext]'
-                }
-              }
-            ]
-          }
-        ]
-      }
-    }
-  }
+        module: {
+            rules: [{
+                test: /\.pdf$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: 'files/[name].[hash:8].[ext]'
+                    }
+                }]
+            }]
+        }
+    },
+    outputDir: '../static/_cov',
+    publicPath: './'
+}
